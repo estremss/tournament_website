e37ef8489be7c39eef855e8adef1c60eee615ea4
estremss <estrems.evan@gmail.com>
2024-04-13 19:57:16 +0200

    avancement page connexion

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 console.log("test");
 
-document.getElementById("boutonCommencer").addEventListener("click", function() {location.href = "..\\pages\\tournoi.html";});
+document.getElementById("boutonCommencer").addEventListener("click", function () { location.href = "../pages/connexion.html"; });
+
+function validateForm() {
+    var username = document.getElementById("username").value;
+    var userInput = document.getElementById('captchaInput').value.toLowerCase();
+    var monExprRegLetter = /^[A-Za-z]+$/
+
+    if (!username.match(monExprRegLetter)) {
+        console.log(3);
+        document.getElementById("usernameError").innerHTML = "Le nom d'utilisateur doit contenir uniquement des lettres.";
+        return false;
+    } else {
+        document.getElementById("usernameError").innerHTML = "";
+    }
+
+    if (userInput != 'smwm') {
+        alert('Captcha incorrect. Veuillez réessayer.');
+        return false
+    }
+
+    return true;
+}

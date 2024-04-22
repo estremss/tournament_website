04b3a8e7aaf44d834c437b96d8c15b99c55cdc52
estremss <estrems.evan@gmail.com>
2024-04-22 22:09:37 +0200

    architecture projet + détails menu + début page joueur

--- script.js
+++ script.js
@@ -27,3 +27,16 @@ function validateForm() {
 function Redirection() {
     location.href = "../pages/connexion.html";
 }
+
+function Deconnexion() {
+        // event.preventDefault();
+
+        var confirmation = confirm("Êtes-vous sûr de vouloir vous déconnecter ?");
+
+        if (confirmation) {
+            //window.location.href = deconnexionLink.href;
+            return true;
+        }
+
+        return false;
+    }
